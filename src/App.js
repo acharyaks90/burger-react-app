bc6540dc86a378ac61233dc9d1872da9754066eb
anil suryavanshi <thakurinfotech@gmail.com>
2018-05-26 11:48:57 +0530

    Burger Builder Component added and imported in layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import Layout from './Layout/Layout'
+import Layout from './Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 
 class App extends Component {
   render() {
     return (
       <div >
         <Layout>
-          <p>test</p>
+            <BurgerBuilder/>
         </Layout>
       </div>
     );
