c38f56c7c22762de6cf6715f3aa211dfcb980220
anil suryavanshi <thakurinfotech@gmail.com>
2018-05-28 21:36:15 +0530

    Added Dynamic Ingredient and Custom message

--- src/Components/Burger/Burger.js
+++ src/Components/Burger/Burger.js
@@ -5,17 +5,22 @@ import  BurgerIngredient from './BurgerIngredient/BurgerIngredients';
 
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igkey=>{
             return [...Array(props.ingredients[igkey])].map((_,i)=>{
               return  <BurgerIngredient key={igkey + i} type={igkey}/>
 
-            });
-        });
+            })
+
+        }).reduce((arr, el)=>{
+            return arr.concat(el)
+        },[]);
+    if(transformedIngredients.length===0){
+        transformedIngredients = <p>please add ingredients</p>
+    }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-
             {transformedIngredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
