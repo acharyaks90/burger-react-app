0dd957f136ce0fb688c206b56f5b5498f286337d
anil suryavanshi <thakurinfotech@gmail.com>
2018-05-28 19:56:16 +0530

    Added Dynamic Burger

--- src/Components/Burger/Burger.js
+++ src/Components/Burger/Burger.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import classes from './Burger.css';
-
 import  BurgerIngredient from './BurgerIngredient/BurgerIngredients';
 
+
+
 const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igkey=>{
+            return [...Array(props.ingredients[igkey])].map((_,i)=>{
+              return  <BurgerIngredient key={igkey + i} type={igkey}/>
+
+            });
+        });
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
+
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );
