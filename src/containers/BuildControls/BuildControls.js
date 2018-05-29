488f0d7f028e4d78e9a80ecbb7ded16b67b6b0b3
anil suryavanshi <thakurinfotech@gmail.com>
2018-05-29 21:29:42 +0530

    Added Price to Build Control

--- src/containers/BuildControls/BuildControls.js
+++ src/containers/BuildControls/BuildControls.js
@@ -13,6 +13,7 @@ const controls = [
 
 const buildControls = (props)=>{
    return <div className={classes.BuildControls}>
+       <p>Current Price:{props.price.toFixed(2)}</p>
         {controls.map(ctrl=>(
             <BuildControl ingredientAdded={()=>props.ingredientAdded(ctrl.type)}
                           key={ctrl.label} label={ctrl.label}
