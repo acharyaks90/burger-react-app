f9f8c798cda0330328479f5ffcaeeabc2009ff0f
anil suryavanshi <thakurinfotech@gmail.com>
2018-06-02 13:57:41 +0530

    Adding Backdrop functionality

--- src/Components/Navigation/SideDrawer/SideDrawer.js
+++ src/Components/Navigation/SideDrawer/SideDrawer.js
@@ -2,20 +2,25 @@ import React from 'react';
 import classes from './SideDrawer.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from  '../NavigationItems/NavigationItems';
+import Aux from '../../../hoc/Aux';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 
 const sideDrawer = (props) =>{
     //..
 
     return (
-        <div className={classes.SideDrawer}>
-            <div className={classes.Logo}>
-                <Logo  />
-            </div>
+        <Aux>
+            <Backdrop show> </Backdrop>
+            <div className={classes.SideDrawer}>
+                <div className={classes.Logo}>
+                    <Logo  />
+                </div>
 
-            <nav>
-                <NavigationItems/>
-            </nav>
-        </div>
+                <nav>
+                    <NavigationItems/>
+                </nav>
+            </div>
+        </Aux>
     )
 }
 
